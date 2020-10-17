f6ad058826c36c38fb3b8ade9f7aae8960670561
dinesh01988 <72983957+dinesh01988@users.noreply.github.com>
2020-10-17 08:17:05 +0530

    Update HelloWorld.java

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -3,5 +3,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+string msg="Hello World" ;
+        System.out.println(msg) ;
     }
 }
